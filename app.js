0d1f6a85b9dfbb4fae92d9d42e8cf2cd8e9bb096
ngrigorev <gori.chtobi.svetit@mail.ru>
2020-02-06 23:57:48 +0300

    set view engine set request parser set public folder for client

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
-let app = express();
 let port = 80;
+let app = express();
+
+app.set('view engine', 'pug');
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: true }));
 
 app.listen(port, () => {
     console.info(`App run on http://localhost:${port}`);

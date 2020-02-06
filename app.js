279a48711e1b20e264e92c00bafc3b375b304471
ngrigorev <gori.chtobi.svetit@mail.ru>
2020-02-07 00:38:31 +0300

    add main '/' route add main page with layout

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const routes = require('./routes');
 
 let port = 80;
 let app = express();
@@ -8,6 +9,7 @@ app.set('view engine', 'pug');
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: true }));
+app.use('/', routes);
 
 app.listen(port, () => {
     console.info(`App run on http://localhost:${port}`);

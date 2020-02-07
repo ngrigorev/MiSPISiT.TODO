7ded8d6b075a2d82d151962510a342b7dc79f1ef
ngrigorev <gori.chtobi.svetit@mail.ru>
2020-02-07 12:14:19 +0300

    create models

---
+++ models/ViewModels.js
@@ -0,0 +1,25 @@
+exports.Task = class {
+    /**
+     * @param {String} title 
+     * @param {Date} start 
+     * @param {Date} end 
+     * @param {String} status 
+     */
+    constructor(title, start, end, status){
+        this.title = title || '';
+        this.startDate = start || new Date();
+        this.endDate = end || new Date();
+        this.status = !!status;
+    }
+}
+
+exports.MainViewModel = class {
+    /**
+     * @param {String} title Title
+     * @param {Array<Task>} tasks Array of Tasks
+     */
+    constructor(title, tasks){
+        this.title = title || '';
+        this.tasks = tasks || [];
+    }
+}

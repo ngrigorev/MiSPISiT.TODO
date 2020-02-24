4f81c80661adf009e00804f515e880a5bdb32dc5
ngrigorev <gori.chtobi.svetit@mail.ru>
2020-02-24 21:44:52 +0300

    api

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,9 @@ const main = require('./main');
 const crud = require('./crud');
 const api = require('./api');
 
-router.get('/', main);
-router.get('/get/:id', crud.get);
-router.post('/add', crud.add);
-router.post('/update', crud.update);
-router.get('/delete/:id', crud.delete);
+router.get('/', (r, q) => {
+    q.redirect('/api');
+});
 
 router.get('/api', api.info);
 router.get('/api/tasks', api.login, api.get);

f3dbda17ad924ffece6b2df0c435785b9d3f1df5
ngrigorev <gori.chtobi.svetit@mail.ru>
2020-02-24 22:44:05 +0300

    prepare api

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
 
-let port = 80;
+let port = process.env.PORT || 3000;
 let app = express();
 
 app.set('view engine', 'pug');
